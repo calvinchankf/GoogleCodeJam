7f1d282bf74a7217caa3b694624110e288fc1966
calvinchankf <chan9118kin@gmail.com>
2020-04-04 15:39:34 +0800

    2020 q e: should fail large dataset

--- 2020/qualification/e.py
+++ 2020/qualification/e.py
@@ -102,19 +102,19 @@ class Board(object):
             temp.append(row[:])
         return temp
 
-# input() reads a string with a line of input, stripping the ' ' (newline) at the end.
-# This is all you need for most Code Jam problems.
-T = int(input())  # read a line with a single integer
-for t in range(1, T + 1):
-    N, K = [int(s) for s in raw_input().split(" ")]
-    s = Solution()
-    grid = s.generateAny(N, K)
-    if grid == None:
-        print("Case #{}: IMPOSSIBLE".format(t))
-    else:
-        print("Case #{}: POSSIBLE".format(t))
-        for row in grid:
-            print(" ".join([str(x) for x in row]))
+# # input() reads a string with a line of input, stripping the ' ' (newline) at the end.
+# # This is all you need for most Code Jam problems.
+# T = int(input())  # read a line with a single integer
+# for t in range(1, T + 1):
+#     N, K = [int(s) for s in raw_input().split(" ")]
+#     s = Solution()
+#     grid = s.generateAny(N, K)
+#     if grid == None:
+#         print("Case #{}: IMPOSSIBLE".format(t))
+#     else:
+#         print("Case #{}: POSSIBLE".format(t))
+#         for row in grid:
+#             print(" ".join([str(x) for x in row]))
 
 print("-----")
 
@@ -135,3 +135,7 @@ print(s.generateAny(4, 16))
 
 s = Solution()
 print(s.generateAny(5, 25))
+
+# too long, should fail larget dataset
+s = Solution()
+print(s.generateAny(9, 81))
